b23c5985b0902b57d38a44fdd4d6cf98f5a2c7fe
Raktim Banerjee <Raktim@thinksurfmedia.com>
2025-04-25 11:50:28 +0530

    Update

--- app/staff/new-calls/page.tsx
+++ app/staff/new-calls/page.tsx
@@ -112,6 +112,7 @@ export default function NewCallsPage() {
       title: "Calls Assigned",
       description: `${selectedCalls.length} calls have been assigned.`,
     })
+    console.log(selectedCalls)
     setSelectedCalls([])
   }
 
